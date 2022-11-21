59b07c9862f370c536215e5ee7a3c7edc9c811cf
Gregory John <105023491+GregLJ@users.noreply.github.com>
2022-11-20 22:42:41 -0500

    attempt at fixing contact submission test

--- mail/contact.js
+++ mail/contact.js
@@ -1,4 +1,4 @@
-$(function () {
+/*$(function () {
 
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "https://formspree.io/f/xjvzzglg",
                 type: "POST",
                 data: {
                     name: name,
@@ -62,4 +62,35 @@ $(function () {
 
 $('#name').focus(function () {
     $('#success').html('');
-});
+});*/
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
